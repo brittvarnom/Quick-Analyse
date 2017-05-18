ccc5a660aa0a9d65ffcd6f593b90af75fdb0a33c
brittvarnom <brittany.varnom@bbc.co.uk>
2017-05-18 10:00:28 +0100

    Activities for text, file and demo. Buttons switch to them.@
    @

---
+++ app/src/main/java/britt/com/quickanalyse/DemoInputActivity.kt
@@ -0,0 +1,12 @@
+package britt.com.quickanalyse
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DemoInputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo_input)
+    }
+}
--- app/src/main/java/britt/com/quickanalyse/MainActivity.kt
+++ app/src/main/java/britt/com/quickanalyse/MainActivity.kt
@@ -1,13 +1,32 @@
 package britt.com.quickanalyse
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        onButtonsClicked()
+    }
+
+    fun onButtonsClicked() {
+        onButtonClicked(analyser_text_button, TextInputActivity().javaClass)
+        onButtonClicked(analyser_file_button, FIleInputActivity().javaClass)
+        onButtonClicked(analyser_demo_button, DemoInputActivity().javaClass)
+    }
+
+    fun onButtonClicked(analyser_button: Button, destination: Class<Activity>) {
+        analyser_button.setOnClickListener {
+            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this@MainActivity, destination))
+        }
     }
 }
 
